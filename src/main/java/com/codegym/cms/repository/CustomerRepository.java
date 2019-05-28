ec8ce11cade31329bd5b936dce20aca91b35d5c0
HungMPhung <minhhhunggtvt2011@gmail.com>
2019-05-28 09:16:06 +0700

    fisrt commit

--- src/main/java/com/codegym/cms/repository/CustomerRepository.java
+++ src/main/java/com/codegym/cms/repository/CustomerRepository.java
@@ -2,6 +2,6 @@ package com.codegym.cms.repository;
 
 import com.codegym.cms.model.Customer;
 
-public interface CustomerRepository extends Repository<Customer> {
 
+public interface CustomerRepository extends Repository<Customer> {
 }
